9d8957cce3951c74707e97dbfff2ac60117357ea
Ejex Xeffer <ejexefer@gmail.com>
2019-06-26 00:06:57 +0500

    Make shaker sort

--- C/sorting/shaker/shakerSort.c
+++ C/sorting/shaker/shakerSort.c
@@ -4,10 +4,16 @@
 
 int main()
 {		
-	int size, i, j = 0;
+	int size = 0,
+		i = 0, 
+		j = 0, 
+		left = 1,
+		count = 0,
+		last = 0;
 	// input values
 	printf("Please, size of an array.\n");
 	scanf("%d", &size);
+
 	if (size <= 0)
 	{
 		printf("Size must be positive value.\n");
@@ -15,41 +21,53 @@ int main()
 	}
 
 	int bls[size];
+	int right = size-1;
 
 	for (i=0; i < size; i++)
 	{
 		printf("Please, enter value: ");
 		scanf("%d", &bls[i]);
 	}
-
+	
 	// export array
 	printf("This is your array: ");
-	for (i=0; i < size; i++) 
+	for (i=0; i < size; i++)
 	{
 		printf("%d ", bls[i]);
 	}
 	printf("size=%d\n", size);
 	printf("\n");
 
-	/*// size of array (for strings)
-	for (i = 1; bls[i]!='\0'; i++)
-	{
-		size = i;
-	}*/
-
 	// sorting
-	for (j = 0; j < size; j++)
+	 do
 	{
-		for (i = 0; i < size; ++i)
+		for(i = right; i >= left; i--)
 		{
-			if (bls[i]>bls[i+1])
+			if(bls[i-1] > bls[i])
 			{
-			bls[i]=bls[i]^bls[i+1];
-			bls[i+1]=bls[i]^bls[i+1];
-			bls[i]=bls[i]^bls[i+1];
+				bls[i]=bls[i]^bls[i-1];
+				bls[i-1]=bls[i]^bls[i-1];
+				bls[i]=bls[i]^bls[i-1];
+				last = i;
 			}
 		}
-	}
+
+		left = last + 1;
+
+		for(i = left; i <= right; i++)
+		{
+			if(bls[i-1] > bls[i])
+			{
+				bls[i]=bls[i]^bls[i-1];
+				bls[i-1]=bls[i]^bls[i-1];
+				bls[i]=bls[i]^bls[i-1];
+				last = i;
+			}
+		}
+
+		right = last-1;
+
+	} while(left < right);
 	
 	// export array
 	printf("This is your array: ");
@@ -57,7 +75,7 @@ int main()
 	{
 		printf("%d ", bls[i]);
 	}
-	printf("size=%d\n", size);
+	printf("size=%d\n", size);;
 	printf("\n");
 
 	return 0;
