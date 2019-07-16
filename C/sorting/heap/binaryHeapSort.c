0685d064b1cad449a65168a46aae917155853500
Ejex Xeffer <ejexefer@gmail.com>
2019-07-16 23:19:26 +0500

    make heap sort great again

--- C/sorting/heap/binaryHeapSort.c
+++ C/sorting/heap/binaryHeapSort.c
@@ -24,32 +24,63 @@ void change (int *array, int i, int j) {
 	array[j]=array[i]^array[j];
 	array[i]=array[i]^array[j];
 }
+
+int maxVal (int *array, int a, int b) {
+	return array[a] > array[b] ? a : b;
+}
+
+int maxHeap (int *array, int size, int i) {
+	int j = 0;
+	int m = 2*i+1;
+	if((m)<size && (m+1)<size) {
+		j=maxVal(array,m, m+1);
+	} else if (m<size) {
+		j=m;
+	} else if (m+1<size) {
+		j=m+1;
+	} else j=i;
+	j = maxVal(array, i, j);
+	if (j!=i){
+		change(array, i, j);
+		return j;
+	} else {
+		return -1;
+	}
+}
+
+
 //sorting child is elements from parent
-void maxHeap (int *array, int size, int i) {
+void heap(int *array, int size, int i) {
 	int j = i;
-	if (2*i+1<size && array[2*i+1]>array[i]) j=2*i+1;
-	if (2*i+2<size && array[2*i+2]>array[i]) j=2*i+2;
-	if (j!=i) change(array,j,i);
+	exportArray(array,size);
+	do {
+		printf("j=%d\n", j);
+		printf("i=%d\n", i);
+		j = maxHeap(array,size,j);
+	} while (j<=size/2-1 && j!=-1);
 }
 
 void heapSort (int *array, int size) {
 	//make heap
-	for (int i=size/2; i>=0; i--) {
-		maxHeap(array, size, i);
+	for (int i=size/2-1; i>=0; i--) {
+		printf("i=%d\n", i);
+		heap(array, size, i);
 	}
-	int last = size;
-	while (last>1) {
-		last--;
+	exportArray(array,size);
+	printf("*********END HEAP*********\n");
+	int last = size - 1;
+	printf("*********BEGIN SORTING*********\n");
+	while (last>0) {
 		change(array, 0, last);
-
-		heapSort(array,last);
+		heap(array,last, 0);
+		exportArray(array,size);
+		printf("*********END STEP*********\n");		
+		last --;	
 	}
 }
 
 int main() {
-	int size, i, j, key, m = 0;
-	int count = 1;
-	int countBuffer = 0;
+	int size = 0;
 	printf("Please, size of an array.\n");
 	scanf("%d", &size);
 	//check value size
@@ -66,6 +97,7 @@ int main() {
 	// sort array of heap
 	heapSort(bls,size);
 	// export sorted array
+	printf("*********END SORTING*********\n");
 	exportArray(bls,size);
 	return 0;
 }
